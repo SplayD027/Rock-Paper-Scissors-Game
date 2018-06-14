e8a8841317769b8ee0df85c2a4669405e31fd04d
punpun sitti <38572855+SplayD027@users.noreply.github.com>
2018-06-15 04:54:21 +0700

    add reset button and delete unnecessary UI.

--- script/game.js
+++ script/game.js
@@ -1,3 +1,15 @@
+let count = 0;
+let win = 0;
+let lose = 0;
+
+let buttons = Array.from(document.querySelectorAll('.buttons button'));
+buttons.forEach(button => button.addEventListener('click',play));
+buttons.forEach(button => button.addEventListener('click',function(){
+    document.querySelector('#reset').style.display = "block";
+}));
+let resetButton = document.querySelector('#reset');
+resetButton.addEventListener('click',reset);
+
 function computerPlay(){
   function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
@@ -14,83 +26,74 @@ function computerPlay(){
   }
 }
 
+function showResult(player,ai,result){
+    console.log(player == "");
+    if(!(player == "")){
+       player = player[0].toUpperCase() + player.slice(1);
+    }
+//    document.querySelector("#ply").innerText = player;
+    document.querySelector("#whoWon").innerText = result;
+//    document.querySelector("#ai").innerText = ai;
+    return;
+}
+
 function game(playerSelection,computerSelection){
   if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-    document.querySelector(".text p").innerText = "Tie! You Both Chose" + computerSelection;
+    showResult(playerSelection,computerSelection,"Draw");
     return;
   }
+    showResult("","","");
   switch(playerSelection.toLowerCase()){
     case "paper":
       if (computerSelection == "Rock"){
-        document.querySelector(".text p").innerText = "You Won! Paper beats Rock";
+//        showResult(playerSelection,computerSelection,"You Won!");
         return 1;
       } else {
-        document.querySelector(".text p").innerText = "You Lose! Scissors beats Paper";
+//        showResult(playerSelection,computerSelection,"You Lose!");
         return 0;
       }
     case "rock":
     if (computerSelection == "Scissors"){
-      document.querySelector(".text p").innerText = "You Won! Rock beats Scissors";
+//      showResult(playerSelection,computerSelection,"You Won!");
       return 1;
     } else {
-      document.querySelector(".text p").innerText = "You Lose! Paper beats Rock";
+//      showResult(playerSelection,computerSelection,"You Lose!");
       return 0;
     }
     case "scissors":
     if (computerSelection == "Paper"){
-      document.querySelector(".text p").innerText = "You Won! Scissors beats Paper";
+//      showResult(playerSelection,computerSelection,"You Won!");
       return 1;
     } else {
-      document.querySelector(".text p").innerText = "You Lose! Rock beats Scissors";
+//      showResult(playerSelection,computerSelection,"You Lose!");
       return 0;
     }
  }
 }
-/*
+
 function play(a){
-  console.log('Best of 5!');
-  let result = 0,win = 0,lose = 0,playerSelection = "";
-  for (let i=0;i!=5;i++){
-    playerSelection = a.target.id;
-    console.log(playerSelection);
-      if (playerSelection != "paper" && playerSelection != "rock" && playerSelection != "scissors"){
-        console.log('Please type in "Paper" "Rock" "Scissors"');
-        return;
-      }
-    result = game(playerSelection,computerPlay());
-    (result == 1) ? win++ : (result == 0) ? lose++ : "";
-  }
-  if (win > lose){
-    console.log("You Won! Score: " + win);
-  }else{
-    console.log("You Lose! Score: " + win);
-  }
-}
-*/
-function play(a){
-  if (win == 5 || lose == 5){
-    document.querySelector(".text h1").innerText = "YOU WON!";
-    if (lose == 5) document.querySelector(".text h1").innerText = "YOU LOSE!";
-    return;
-  }
-  if (!(win == 5 || lose == 5)) count++;
+  if (!(win == 5 || lose == 5)){
+  count++;
   document.querySelector("#round").innerText = "Round " + count;
-  let playerSelection = a.target.id;
-  console.log(playerSelection);
-
-  let result = game(playerSelection,computerPlay());
+  let result = game(a.target.id,computerPlay());
   (result == 1) ? win++ : (result == 0) ? lose++ : "";
-
   document.querySelector("#win").innerText = win;
   document.querySelector("#lose").innerText = lose;
+  }
+  if (win == 5 || lose == 5){
+    document.querySelector("#title").innerText = "YOU WON!";
+    if (lose == 5) document.querySelector("#title").innerText = "YOU LOSE!";
+    document.querySelector("#title").innerHTML 
+    return;
+  }
 }
 
-let buttons = Array.from(document.querySelectorAll('.buttons button'));
-buttons.forEach(button => button.addEventListener('click',play));
-
-let text = document.querySelector(".text h1");
-let smallText = document.querySelector(".text p");
-
-let count = 0;
-let win = 0;
-let lose = 0;
+function reset(){
+    count = 0,win = 0,lose = 0;
+    document.querySelector("#round").innerText = ""
+    document.querySelector("#win").innerText = win;
+    document.querySelector("#lose").innerText = lose;
+    showResult("","","");
+    document.querySelector("#title").innerText = "Rock Paper Scissors";
+    document.getElementById('reset').style.display = "none";
+}
